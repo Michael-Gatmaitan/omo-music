00cc19406dd5e0c33b289545c9406304d0c71432
root <root@DESKTOP-M2NOFCG>
2024-08-23 02:17:19 +0800

    $uicideboy$ and Novulent added 🙌🏻

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
     css: true,
-    reporters: ['verbose'],
+    reporters: ["verbose"],
     coverage: {
-      reporter: ['text', 'json', 'html'],
-      include: ['src/**/*'],
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*"],
       exclude: [],
-    }
+    },
   },
-})
+});
+
